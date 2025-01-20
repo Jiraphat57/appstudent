97b2c21228eb4e58d0425a8df8bf3f868dff7fcc
จิรภัทร <jiraphat@phanphit.ac.th>
2025-01-20 09:21:49 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,20 +18,39 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(ProvinceSeeder::class);
-        $this->call(BloodtypeSeeder::class);
-        $this->call(EthnicitySeeder::class);
-        $this->call(GenderSeeder::class);
-        $this->call(MaritalstatusSeeder::class);
-        $this->call(NationalitySeeder::class);
-        $this->call(ReligionSeeder::class);
-        $this->call(SchoolbreakSeeder::class);
-        $this->call(TyperesidenceSeeder::class);
-        $this->call(TypetitleSeeder::class);
-        $this->call(OccupationSeeder::class);
-        $this->call(ClassLevelSeeder::class);
-        $this->call(HighSchoolSeeder::class);
-        $this->call(SecondarySchoolSeeder::class);
-        $this->call(AdminSeeder::class);
+
+        // $this->call(ProvinceSeeder::class);
+        // $this->call(BloodtypeSeeder::class);
+        // $this->call(EthnicitySeeder::class);
+        // $this->call(GenderSeeder::class);
+        // $this->call(MaritalstatusSeeder::class);
+        // $this->call(NationalitySeeder::class);
+        // $this->call(ReligionSeeder::class);
+        // $this->call(SchoolbreakSeeder::class);
+        // $this->call(TyperesidenceSeeder::class);
+        // $this->call(TypetitleSeeder::class);
+        // $this->call(OccupationSeeder::class);
+        // $this->call(ClassLevelSeeder::class);
+        // $this->call(HighSchoolSeeder::class);
+        // $this->call(SecondarySchoolSeeder::class);
+        // $this->call(AdminSeeder::class);
+
+        $this->call([
+            ProvinceSeeder::class,
+            BloodtypeSeeder::class,
+            EthnicitySeeder::class,
+            GenderSeeder::class,
+            MaritalstatusSeeder::class,
+            NationalitySeeder::class,
+            ReligionSeeder::class,
+            SchoolbreakSeeder::class,
+            TyperesidenceSeeder::class,
+            TypetitleSeeder::class,
+            OccupationSeeder::class,
+            ClassLevelSeeder::class,
+            HighSchoolSeeder::class,
+            SecondarySchoolSeeder::class,
+            AdminSeeder::class
+        ]);
     }
 }
