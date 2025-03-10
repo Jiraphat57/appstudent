c47dde2ec12b447da7b3f8495c3d620bc6f8f590
จิรภัทร <jiraphat@phanphit.ac.th>
2025-03-10 16:31:14 +0700

    แก้7

--- app/Http/Controllers/Student4Controller.php
+++ app/Http/Controllers/Student4Controller.php
@@ -160,7 +160,7 @@ class Student4Controller extends Controller
             'highschool10_id' => 'required|numeric'
             // 'highschool11_id' => 'required|numeric' 
         ]);
-        dd($validatedData);
+        // dd($validatedData);
         // แปลงรูปแบบวันที่
         try {
             list($day, $month, $year) = explode('/', $validatedData['dateofbirth']);
@@ -181,7 +181,7 @@ class Student4Controller extends Controller
         $students->nationalid = $request->input('nationalid');
         $students->religions_id = $request->input('religions_id');
         $students->nationalities_id = $request->input('nationalities_id');
-        $students->phone4student = $request->input('phone4student');
+        // $students->phone4student = $request->input('phone4student');
         $students->ethnicities_id = $request->input('ethnicities_id');
         $students->dateofbirth = $dateOfBirth; // ใช้วันที่ที่แปลงแล้ว
         $students->provincesbirth_id = $request->input('provincesbirth_id');
