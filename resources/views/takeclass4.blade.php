26da310d4495d88c37f3c9fe19cc56eaaf23fde1
จิรภัทร <jiraphat@phanphit.ac.th>
2025-03-16 12:46:42 +0700

    แก้6

--- resources/views/takeclass4.blade.php
+++ resources/views/takeclass4.blade.php
@@ -199,7 +199,7 @@
 
                 <div class="col-md-6 mb-2">
                     <div class="input-group date" id="datepicker">
-                        <label for="inputdate" class="col-sm-3 col-form-label">วันเกิดนักเรียน
+                        <label for="inputdate" class="col-sm-3 col-form-label">วันเกิดนักเรียน</label>
                         <input type="text" name="dateofbirth" class="form-control" placeholder="วันเกิดนักเรียน">
                         <span class="input-group-text">
                             <i class="bi bi-calendar"></i>
