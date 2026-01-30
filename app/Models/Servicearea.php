a9856fea846431ddfa15ee52da2d03efa0614bc3
จิรภัทร <jiraphat@phanphit.ac.th>
2026-01-30 08:44:52 +0700

    6

--- app/Models/Servicearea.php
+++ app/Models/Servicearea.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Students;
+use App\Models\Student4;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
